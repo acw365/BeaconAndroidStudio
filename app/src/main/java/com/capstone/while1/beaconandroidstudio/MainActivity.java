2209b1b616599746839565beea340d2deb9beda6
kjmb84 <kjmb84@mail.missouri.edu>
2017-03-19 14:05:14 -0500

    Still trying to figure out the settings page

--- app/src/main/java/com/capstone/while1/beaconandroidstudio/MainActivity.java
+++ app/src/main/java/com/capstone/while1/beaconandroidstudio/MainActivity.java
@@ -9,7 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-        setContentView(R.xml.pref_notification);
+        setContentView(R.layout.activity_settings);
     }
 }
